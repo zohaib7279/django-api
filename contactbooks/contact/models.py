415329f074b48c174e9d79c131a375e84484d9d6
Zohaib Abdul Jabbar <zohaib7279@gmail.com>
2026-02-04 16:53:41 +0500

    register students

--- contactbooks/contact/models.py
+++ contactbooks/contact/models.py
@@ -12,3 +12,6 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
+
+
+    
--- contactbooks/contactbooks/urls.py
+++ contactbooks/contactbooks/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from contact.views import ContactViewSet
 
+
 router = DefaultRouter()
 router.register(r'contact', ContactViewSet, basename='contact')
 
