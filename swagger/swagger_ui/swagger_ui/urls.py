f7e95403efb70cca65b7f1e4276afe93dd8b64f1
Zohaib Abdul Jabbar <zohaib7279@gmail.com>
2026-02-22 16:42:06 +0500

    create swagger docs in api

---
+++ swagger/swagger_ui/swagger_ui/urls.py
@@ -0,0 +1,67 @@
+"""
+URL configuration for swagger_ui project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
+from django.contrib import admin
+from django.urls import path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="My API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('swagger(<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+
+
+from django.urls import path
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+# Dummy GET/POST/DELETE API
+@api_view(['GET', 'POST', 'DELETE'])
+def users_api(request):
+    if request.method == 'GET':
+        return Response({"message": "This is GET response"})
+    elif request.method == 'POST':
+        data = request.data
+        return Response({"message": "This is POST response", "data": data})
+    elif request.method == 'DELETE':
+        return Response({"message": "Deleted successfully"})
+
+urlpatterns += [
+    path('api/users/', users_api, name='users-api'),
+]
