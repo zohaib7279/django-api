c7178bc351b8985f9ae357b241ee9fb17847c91c
Zohaib Abdul Jabbar <zohaib7279@gmail.com>
2026-02-23 17:45:07 +0500

    no work

---
+++ swagger/swagger_ui/swagger_api/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Contact(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.name
